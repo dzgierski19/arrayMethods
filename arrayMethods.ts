504cd8b904e8e5e2f3f3641ca46428793920dac5
dzgierski19 <dzgierskigit@gmail.com>
2023-05-04 22:32:34 +0200

    added every, some, forEach and filter methods

--- arrayMethods.ts
+++ arrayMethods.ts
@@ -1,52 +1,130 @@
-const data = [1, 2, 4, 5];
+const data = [1, 8, 4, 5];
 
 interface Lengthy {
   length: number;
 }
 
-const entriesFn = <T>(arr: T[]): T[] => {
-  const emptyArray: any[] = [];
+const entriesFn = <T>(arr: T[]): [number, T][] => {
+  const emptyArray: [number, T][] = [];
   for (let i = 0; i < arr.length; i++) {
     emptyArray.push([i, arr[i]]);
   }
   return emptyArray;
 };
 
-//nie rozumiem dlaczego nie mozna dać w 8. linijce T
-
 console.log(entriesFn(data));
+
+function* entriesFn2<T>(arr: T[]) {
+  for (let i = 0; i < arr.length; i++) {
+    yield [i, arr[i]];
+  }
+}
+
+const entriesFn3 = entriesFn2(data);
+
+for (let i = 0; i < data.length; i++) {
+  console.log(entriesFn3.next());
+}
 
 const reduceFn = <T>(
   array: T[],
-  callback: (accumulator: T, currentValue: T) => T,
-  initialValue: T
+  callback: (accumulator: T, currentValue: T, index: number) => T,
+  initialValue?: T
 ) => {
-  let accumulator = initialValue;
-  for (let i = 0; i < array.length; i++) {
-    accumulator = callback(accumulator, array[i]);
+  let accumulator = initialValue ? initialValue : array[0];
+  if (accumulator === initialValue) {
+    for (let i = 0; i < array.length; i++) {
+      accumulator = callback(accumulator, array[i], i);
+    }
+    return accumulator;
+  } else {
+    for (let i = 1; i < array.length; i++) {
+      accumulator = callback(accumulator, array[i], i);
+    }
+    return accumulator;
   }
-  return accumulator;
 };
 
 console.log(
   reduceFn(data, (accumulator, currentValue) => accumulator * currentValue, 5)
 );
 
-const mapFn = <T>(array: T[], callback: (element: T) => T) => {
-  const emptyArray: T[] = [];
+const mapFn = <T, K>(
+  array: T[],
+  callback: (element: T, index: number, array: T[]) => K
+) => {
+  const emptyArray: K[] = [];
   for (let i = 0; i < array.length; i++) {
-    emptyArray.push(callback(array[i]));
+    emptyArray.push(callback(array[i], i, array));
   }
   return emptyArray;
 };
 
 console.log(mapFn(data, (element) => element * 2));
-console.log(mapFn(data, (element) => Math.sqrt(element)));
+console.log(mapFn(data, (element) => Number(Math.sqrt(element).toFixed(2))));
 
-// const everyFn = (array, callback) => {};
+function isAllX(string: string) {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "x" || string[i] === "X") {
+      i++;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(isAllX("xxxxxxX"));
 
-// const forEachFn = (array, callback) => {};
+const everyFn = <T>(
+  array: T[],
+  callback: (element: T, index: number) => boolean
+) => {
+  for (let i = 0; i < array.length; i++) {
+    if (!callback(array[i], i)) {
+      return false;
+    }
+  }
+  return true;
+};
 
-// const filterFn = (array, callback) => {};
+console.log(everyFn(data, (element) => element < 10));
 
-// const someFn = (array, callback) => {};
+const someFn = <T>(
+  array: T[],
+  callback: (element: T, index: number) => boolean
+) => {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i)) {
+      return true;
+    }
+  }
+  return false;
+};
+console.log(someFn(data, (element) => element > 11));
+
+const forEachFn = <T>(
+  array: T[],
+  callback: (element: T, index: number) => T
+) => {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i);
+  }
+  return undefined;
+};
+
+console.log(forEachFn(data, (element, index) => element + 1 * index));
+
+const filterFn = <T>(
+  array: T[],
+  callback: (element: T, index: number) => boolean
+) => {
+  const emptyArray: T[] = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i], i)) {
+      emptyArray.push(array[i]);
+    }
+  }
+  return emptyArray;
+};
+
+console.log(filterFn(data, (element) => element % 2 === 0));
